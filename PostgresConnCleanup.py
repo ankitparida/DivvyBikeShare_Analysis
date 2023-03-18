89ff974526874ccfa428a69edf6f27abb5124297
Ankit Parida <30798966+ankitparida@users.noreply.github.com>
2023-03-18 23:59:18 +0530

    Add files via upload

---
+++ PostgresConnCleanup.py
@@ -0,0 +1,26 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+########################################
+# Update connection string information #
+########################################
+host = "udacitypostgres.postgres.database.azure.com"
+user = "udacitypostgresadmin@udacitypostgres"
+password = "Ishu@1701"
+
+# Create a new DB
+sslmode = "require"
+dbname = "postgres"
+conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+conn = psycopg2.connect(conn_string)
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
+print("Connection established")
+
+
+cursor = conn.cursor()
+cursor.execute("SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'udacityproject' AND pid <> pg_backend_pid();")
+
+# Clean up initial connection
+conn.commit()
+cursor.close()
+conn.close()
